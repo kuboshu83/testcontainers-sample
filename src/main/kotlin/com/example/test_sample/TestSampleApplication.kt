5ceaf16948e1d35704840533d9a78cb224300cca
kuboshu83 <dot.sohkubo@gmail.com>
2026-03-07 10:44:49 +0900

    first commit

---
+++ src/main/kotlin/com/example/test_sample/TestSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.test_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/test_sample/TestSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.test_sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

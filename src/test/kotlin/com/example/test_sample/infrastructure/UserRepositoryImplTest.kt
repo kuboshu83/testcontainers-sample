73f5449182f493f57b5a244bbacc77bb0a17a2b6
kuboshu83 <dot.sohkubo@gmail.com>
2026-03-08 16:29:27 +0900

    @ServiceConnectionを使用するようにした

--- src/test/kotlin/com/example/test_sample/infrastructure/UserRepositoryImplTest.kt
+++ src/test/kotlin/com/example/test_sample/infrastructure/UserRepositoryImplTest.kt
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.transaction.annotation.Transactional
 import org.testcontainers.junit.jupiter.Container
@@ -24,18 +23,11 @@ class UserRepositoryImplTest() {
 
     companion object {
         @Container
+        @ServiceConnection
         private val postgres = PostgreSQLContainer("postgres:18.3").apply {
             withUsername("postgres")
             withPassword("postgres")
             withDatabaseName("db")
-        }
-
-        @JvmStatic
-        @DynamicPropertySource
-        fun registerProps(registry: DynamicPropertyRegistry) {
-            registry.add("spring.datasource.url") { postgres.jdbcUrl }
-            registry.add("spring.datasource.username") { postgres.username }
-            registry.add("spring.datasource.password") { postgres.password }
         }
     }
 

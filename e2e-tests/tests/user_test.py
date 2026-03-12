7831e854c1a4fcf36d652ae71a5d680925687039
kuboshu83 <dot.sohkubo@gmail.com>
2026-03-12 12:00:01 +0900

    Playwrightが一応動くようにした

---
+++ e2e-tests/tests/user_test.py
@@ -0,0 +1,13 @@
+import time
+from playwright.sync_api import sync_playwright
+
+BASE_URL = "http://app-test:8080"
+
+
+def test_crud():
+    time.sleep(20)
+    with sync_playwright() as p:
+        api = p.request.new_context(base_url=BASE_URL)
+        res = api.get("/api/users")
+        assert not res.ok
+        assert res.status == 500
